ad74b8863102a60921f36dfcabbe694f84ac33b6
安小游 <471607346@qq.com>
2019-05-20 17:56:50 +0800

    commit

--- application/h5/controller/Upload.php
+++ application/h5/controller/Upload.php
@@ -18,16 +18,12 @@ class Upload extends Controller{
         // 移动到框架应用根目录/uploads/ 目录下
         $info = $file->move( './uploads');
         if($info){
-            // 成功上传后 获取上传信息
-            // 输出 jpg
-            echo $info->getExtension();
+            return json_url(['code' => 200,'msg' => 'ok']);
             // 输出 20160820/42a79759f284b767dfcb2a0197904287.jpg
-            echo $info->getSaveName();
-            // 输出 42a79759f284b767dfcb2a0197904287.jpg
-            echo $info->getFilename();
+            //echo $info->getSaveName();
         }else{
+            return json_url(['code' => 400,'msg' => 'ok']);
             // 上传失败获取错误信息
-            echo $file->getError();
         }
     }
 }
