4bf2b1731c275493fedcfaef97c06637d60ddd3b
安小游 <471607346@qq.com>
2019-05-20 14:21:07 +0800

    commit

--- application/h5/controller/Index.php
+++ application/h5/controller/Index.php
@@ -3,16 +3,63 @@
 namespace app\h5\controller;
 
 use think\Controller;
+
 /**
  * Created by PhpStorm.
- * User: yinchuanjiang
- * Date: 2019/5/20
- * Time: 下午1:47
+ * User=>yinchuanjiang
+ * Date=>2019/5/20
+ * Time=>下午1:47
  */
-class Index extends Controller{
+class Index extends Controller
+{
     //h5活动首页
     public function index()
     {
         return $this->fetch();
+    }
+
+    //列表页
+    public function lists()
+    {
+        $data = [
+            [
+                "id" => 2,
+                "uuid" => "ba2b10e8-0f01-4483-ba66-10756929d2b3",
+                "avatar" => "http://wx.qlogo.cn/mmopen/vi_32/Q0j4TwGTfTLWiaE1p4KnCEXLxTu3BOk4OBic0PeBX2kLWF2WAWx3x9yF5kHUx3QD6T5mHhnZYWO5ibmCcx6c3KfGQ/0",
+                "nickname" => "Suzanne",
+                "number" => 2398,
+                "poster" => "http://static.diggid.cn/FrJYktkt7tF-aK_YVofFyzy-i_2U",
+                "master" => false,
+                "liked" => false
+            ], [
+                "id" => 2,
+                "uuid" => "81ae7659-1d2d-4601-89d8-c3bf72d3bf38",
+                "avatar" => "http://wx.qlogo.cn/mmopen/vi_32/DYAIOgq83eoR7HdY3fRWuvXibqebLNXx0LMaqAHiafw7uoJsRX3oOMtgj2lQDSvx9sPibJkPaCnb7QkYaHnDWAntA/0",
+                "nickname" => "大牛",
+                "number" => 1644,
+                "poster" => "http://static.diggid.cn/Fgxng5zbVpx5OGAh-SvMlVHJtbn_",
+                "master" => false,
+                "liked" => false
+            ], [
+                "id" => 3,
+                "uuid" => "59c1d63f-11c6-400d-b322-5c25883d32ee",
+                "avatar" => "http://wx.qlogo.cn/mmopen/vi_32/DYAIOgq83eqzwYzVu4jk1vSu5dnbuIzqmBRiaXicPriaGGc4N7RgbZORibZWiakpFc30l2w5GmJp0HhIiboiaicRhRZ3kw/0",
+                "nickname" => "u65b9u51ef",
+                "number" => 949,
+                "poster" => "http://static.diggid.cn/Fs1EJbG3xIXOHrQaDlLB8A9ExOYd",
+                "master" => false,
+                "liked" => false
+            ], [
+                "id" => 4,
+                "uuid" => "077531fb-dcce-45e7-af56-b3d680599565",
+                "avatar" => "http://wx.qlogo.cn/mmopen/vi_32/DYAIOgq83eqzwYzVu4jk1vSu5dnbuIzqmBRiaXicPriaGGc4N7RgbZORibZWiakpFc30l2w5GmJp0HhIiboiaicRhRZ3kw/0",
+                "nickname" => "大大大",
+                "number" => 671,
+                "poster" => "http://static.diggid.cn/FqSGo0HVc01dRuG9PpuY3Ejo1w9e",
+                "master" => false,
+                "liked" => false
+            ]
+        ];
+        return json_url(['code' => 0,'msg' => 'ok','data' => $data]);
     }
 }
---
+++ application/h5/controller/Upload.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yinchuanjiang
+ * Date: 2019/5/20
+ * Time: 下午2:03
+ */
+namespace app\h5\controller;
+
+use think\Controller;
+
+class Upload extends Controller{
+    //上传照片
+    public function upload()
+    {
+        // 获取表单上传文件 例如上传了001.jpg
+        $file = request()->file('image');
+        // 移动到框架应用根目录/uploads/ 目录下
+        $info = $file->move( '../uploads');
+        if($info){
+            // 成功上传后 获取上传信息
+            // 输出 jpg
+            echo $info->getExtension();
+            // 输出 20160820/42a79759f284b767dfcb2a0197904287.jpg
+            echo $info->getSaveName();
+            // 输出 42a79759f284b767dfcb2a0197904287.jpg
+            echo $info->getFilename();
+        }else{
+            // 上传失败获取错误信息
+            echo $file->getError();
+        }
+    }
+}
