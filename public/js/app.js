b0e44bc762ef7c39fc84f6481cbf599b4163cb50
安小游 <471607346@qq.com>
2019-05-21 16:46:16 +0800

    commit

--- public/js/app.js
+++ public/js/app.js
@@ -55927,7 +55927,7 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
             var nickname = __WEBPACK_IMPORTED_MODULE_0__helpers_openid__["a" /* default */].getNickname();
             var that = this;
             alert(openid);
-            axios.post(__WEBPACK_IMPORTED_MODULE_1__config__["a" /* APP_URL */] + 'h5/upload/needUpload', { openid: openid }).then(function (res) {
+            axios.get(__WEBPACK_IMPORTED_MODULE_1__config__["a" /* APP_URL */] + 'h5/upload/needUpload', { openid: openid }).then(function (res) {
                 if (res.data.code == 200) {
                     if (avatar && nickname) {
                         var myfile = $('#file');
