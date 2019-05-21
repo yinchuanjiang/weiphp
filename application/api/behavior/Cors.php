bf313c4b844a2c28f25a78ac51cf1876017345e1
安小游 <471607346@qq.com>
2019-05-21 12:00:14 +0800

    commit

--- application/api/behavior/Cors.php
+++ application/api/behavior/Cors.php
@@ -10,7 +10,7 @@ class Cors
     {
         header('Access-Control-Allow-Origin: *');
         header("Access-Control-Allow-Headers: token,Origin, X-Requested-With, Content-Type, Accept");
-        header('Access-Control-Allow-Methods: POST,GET');
+        header('Access-Control-Allow-Methods: POST,GET,OPTION');
         if (request()->isOptions()) {
             exit();
         }
