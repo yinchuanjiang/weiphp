0c4e2491a6fd3dd96601709042223c2738391da9
安小游 <471607346@qq.com>
2019-05-21 12:02:59 +0800

    commit

--- application/tags.php
+++ application/tags.php
@@ -13,14 +13,15 @@
 return [
     // 应用初始化
     'app_init'     => [
-        'app\\api\\behavior\\Cors'
     ],
     // 应用开始
     'app_begin'    => [],
     // 模块初始化
     'module_init'  => [],
     // 操作开始执行
-    'action_begin' => [],
+    'action_begin' => [
+        'app\\api\\behavior\\Cors'
+    ],
     // 视图内容过滤
     'view_filter'  => [],
     // 日志写入
