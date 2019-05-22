771336c6f4a179f1c3477bec66f2868a48a7266b
安小游 <471607346@qq.com>
2019-05-22 17:32:30 +0800

    commit

---
+++ application/h5/controller/Admin.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yinchuanjiang
+ * Date: 2019/5/22
+ * Time: 下午5:30
+ */
+namespace app\h5\controller;
+
+use think\Controller;
+
+class Admin extends Controller{
+    public function index()
+    {
+        $this->fetch();
+    }
+}
