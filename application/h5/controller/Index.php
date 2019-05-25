c1cf24573187e7aa100b3364ca716ef81814c8ca
安小游 <471607346@qq.com>
2019-05-25 12:05:50 +0800

    commit

--- application/h5/controller/Index.php
+++ application/h5/controller/Index.php
@@ -30,24 +30,8 @@ class Index extends Controller
             abort(404);
         $openid = $data['openid'];
         $this->saveUser($openid);
-        return $this->redirect('/h5/index/home?openid='.$openid);
+        return $this->redirect('/h5/index/home?openid=' . $openid);
     }
-
-    public function home()
-    {
-        $openid = input('openid');
-        if(!$openid)
-            return $this->redirect('/h5/auth/auth');
-        $user = H5User::where('openid',$openid)->find();
-        if(!$user)
-            return $this->redirect('/h5/auth/auth');
-        $this->assign('openid', $user->openid);
-        $this->assign('avatar', $user->avatar);
-        $this->assign('nickname', $user->nickname);
-        return $this->fetch('index');
-    }
-
-    //photo 拍照页面授权
 
     private function saveUser($openid, $avatar = null, $nickname = null)
     {
@@ -67,6 +51,24 @@ class Index extends Controller
             $user->updated_at = date('Y-m-d H:i:s');
             $user->save();
         }
+    }
+
+    //photo 拍照页面授权
+
+    public function home()
+    {
+        dump($this->getSignPackage());die;
+        $openid = input('openid');
+        if (!$openid)
+            return $this->redirect('/h5/auth/auth');
+        $user = H5User::where('openid', $openid)->find();
+        if (!$user)
+            return $this->redirect('/h5/auth/auth');
+        $this->assign('openid', $user->openid);
+        $this->assign('avatar', $user->avatar);
+        $this->assign('nickname', $user->nickname);
+        $this->assign('signPackage',$this->getSignPackage());
+        return $this->fetch('index');
     }
 
     //保存用户信息
@@ -93,7 +95,7 @@ class Index extends Controller
 //        $this->assign('nickname', $user->nickname);
 //        $this->assign('openid', $user->openid);
 //        return $this->fetch('index');
-        return $this->redirect('/h5/index/home?openid='.$openid);
+        return $this->redirect('/h5/index/home?openid=' . $openid);
     }
 
     //列表页
@@ -102,7 +104,7 @@ class Index extends Controller
         if (request()->isOptions()) {
             exit();
         }
-        $page = input('page',1);
+        $page = input('page', 1);
         $type = input('type', 'hot');
         $order = 'id desc';
         if ($type == 'hot') {
@@ -110,21 +112,21 @@ class Index extends Controller
         }
         $cate = input('cate');
         $openid = input('openid');
-        $user = H5User::where('openid',$openid)->find();
-        if(!$user)
-            return show(400,'非法请求');
+        $user = H5User::where('openid', $openid)->find();
+        if (!$user)
+            return show(400, '非法请求');
         if (in_array($type, ['hot', 'new'])) {
-            $data = H5Photo::where('cate', $cate)->where('status',H5PhotoEnum::CHECK_SUCCESS)->order($order)->with('user')->page($page,8)->select();
+            $data = H5Photo::where('cate', $cate)->where('status', H5PhotoEnum::CHECK_SUCCESS)->order($order)->with('user')->page($page, 8)->select();
             //处理今天是否投过票
-            $myVotes = H5PhotoVote::where('vote_user_id',$user->id)->whereTime('created_at','today')->column('h5_photo_id');
-            foreach ($data as &$d){
+            $myVotes = H5PhotoVote::where('vote_user_id', $user->id)->whereTime('created_at', 'today')->column('h5_photo_id');
+            foreach ($data as &$d) {
                 $d->is_voted = false;
-                if(in_array($d->id,$myVotes)){
+                if (in_array($d->id, $myVotes)) {
                     $d->is_voted = true;
                 }
             }
         } else {
-            $data = H5Photo::where('cate', $cate)->where('status',H5PhotoEnum::CHECK_SUCCESS)->where('h5_user_id', $user->id)->order($order)->with('user')->find();
+            $data = H5Photo::where('cate', $cate)->where('status', H5PhotoEnum::CHECK_SUCCESS)->where('h5_user_id', $user->id)->order($order)->with('user')->find();
         }
         return show(200, '获取成功', $data);
     }
@@ -162,19 +164,87 @@ class Index extends Controller
         }
         $id = input('id');
         $openid = input('openid');
-        if(!$id || !$openid)
+        if (!$id || !$openid)
             return show(400, '非法请求');
-        $user = H5User::where('openid',$openid)->find();
-        if(!$user)
-            return show(400,'非法请求');
+        $user = H5User::where('openid', $openid)->find();
+        if (!$user)
+            return show(400, '非法请求');
         $photo = H5Photo::find($id);
-        if($photo->cate == 'photo')
+        if ($photo->cate == 'photo')
             $photo->load('user');
-        $myVotes = H5PhotoVote::where('vote_user_id',$user->id)->whereTime('created_at','today')->column('h5_photo_id');
+        $myVotes = H5PhotoVote::where('vote_user_id', $user->id)->whereTime('created_at', 'today')->column('h5_photo_id');
         $photo->is_voted = false;
-        if(in_array($photo->id,$myVotes)){
+        if (in_array($photo->id, $myVotes)) {
             $photo->is_voted = true;
         }
         return show(200, '获取成功', $photo);
+    }
+
+
+    /**
+     * 初始化方法 判断是否登录 未登录跳转到登录页
+     */
+
+    private function getSignPackage()
+    {
+        $jsapiTicket = $this->getJsApiTicket();
+
+        // 注意 URL 一定要动态获取，不能 hardcode.
+        $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
+        $url = "$protocol$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+
+        $timestamp = time();
+        $nonceStr = $this->createNonceStr();
+
+        // 这里参数的顺序要按照 key 值 ASCII 码升序排序
+        $string = "jsapi_ticket=$jsapiTicket&noncestr=$nonceStr&timestamp=$timestamp&url=$url";
+
+        $signature = sha1($string);
+
+        $signPackage = array(
+            "appId" => $this->appid,
+            "nonceStr" => $nonceStr,
+            "timestamp" => $timestamp,
+            "url" => $url,
+            "signature" => $signature,
+            "rawString" => $string
+        );
+        return $signPackage;
+    }
+
+    private function getJsApiTicket()
+    {
+        $accessToken = get_access_token();
+        // 如果是企业号用以下 URL 获取 ticket
+        // $url = "https://qyapi.weixin.qq.com/cgi-bin/get_jsapi_ticket?access_token=$accessToken";
+        $url = "https://api.weixin.qq.com/cgi-bin/ticket/getticket?type=jsapi&access_token=$accessToken";
+        $res = json_decode($this->httpGet($url));
+        $ticket = $res->ticket;
+        return $ticket;
+    }
+
+    private function httpGet($url)
+    {
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_TIMEOUT, 500);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($curl, CURLOPT_URL, $url);
+
+        $res = curl_exec($curl);
+        curl_close($curl);
+
+        return $res;
+    }
+
+    private function createNonceStr($length = 16)
+    {
+        $chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        $str = "";
+        for ($i = 0; $i < $length; $i++) {
+            $str .= substr($chars, mt_rand(0, strlen($chars) - 1), 1);
+        }
+        return $str;
     }
 }
