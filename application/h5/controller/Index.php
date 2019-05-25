8744fe3e76357fbf299988a5dae958a974ac8c78
安小游 <471607346@qq.com>
2019-05-25 12:18:11 +0800

    commit

--- application/h5/controller/Index.php
+++ application/h5/controller/Index.php
@@ -192,7 +192,7 @@ class Index extends Controller
         // 注意 URL 一定要动态获取，不能 hardcode.
         $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
         $url = "$protocol$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
-        dump($url);
+        dump($_SERVER['HTTP_HOST']);
 
         $timestamp = time();
         $nonceStr = $this->createNonceStr();
