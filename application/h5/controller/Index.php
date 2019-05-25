2791a6d669b17c119319c4d6f225d1c27a210b78
安小游 <471607346@qq.com>
2019-05-25 13:07:45 +0800

    commit

--- application/h5/controller/Index.php
+++ application/h5/controller/Index.php
@@ -190,7 +190,7 @@ class Index extends Controller
 
         // 注意 URL 一定要动态获取，不能 hardcode.
         $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
-        $url = "$protocol$_SERVER[HTTP_HOST]"."/h5/auth/auth";
+        $url = "$protocol$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
 
         $timestamp = time();
         $nonceStr = $this->createNonceStr();
@@ -204,7 +204,7 @@ class Index extends Controller
             "appid" => $this->appid,
             "nonceStr" => $nonceStr,
             "timestamp" => $timestamp,
-            "url" => $url,
+            "url" => "$protocol$_SERVER[HTTP_HOST]"."/h5/auth/auth.html",
             'img_url' => "$protocol$_SERVER[HTTP_HOST]"."/static/share.png",
             "signature" => $signature,
             "rawString" => $string
